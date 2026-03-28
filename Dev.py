4192fe51435100ed957f03129cdee4f101edf648
DevTamakuwala16 <devtamakuwala42@gmail.com>
2026-03-28 13:50:55 -0500

    Add files via upload

--- Dev.py
+++ Dev.py
@@ -1,17 +1,37 @@
 new_dict = {}
-for i in range (5):
-    x = input("Create a key: ").lower()
-    y = input("Create a value for that key: ").lower()
-    new_dict[x]=y
-print(new_dict)
-z = input("Call a key: ")
-print(new_dict[z])
-a = input("What key and value would you like to delete: ").lower()
-del new_dict[a]
-print(new_dict)
-print(len(new_dict))
-check = input("Tell me a key and I shall check if it exits").lower()
-if check not in list:
+while True:
     print(new_dict)
-
-second.update(new_dict)
+    x = input("Would you like to create a new key value pair: ").lower()
+    if x == 'yes':
+        d = input("Would you like your value to be a string or a number: ")
+        if d=='string':
+            y = input("Does this key have multiple values: ").lower()
+            if y == "yes":
+                z = input("What is the key: ").lower()
+                v = int(input("How many values would you like to add: "))
+                j = []
+                for i in range (v):
+                    b = input(f'{i+1} value: ')
+                    j.append(b)
+                new_dict[z]=j 
+            elif y =="no":
+                a = input("What would you like your key to be: ").lower()
+                c = input("What would you like your value for the key to be: ").lower()
+                new_dict[a]=c
+        if d == "number":
+            y = input("Does this key have multiple values: ").lower()
+            if y == "yes":
+                e = input("What is the key: ")
+                f = int(input("How many values would you like to add: "))
+                g = []
+                for i in range (f):
+                    b = input(f'{i+1} value: ')
+                    g.append(b)
+                new_dict[e]=g 
+            elif y =="no":
+                h = input("What would you like your key to be: ")
+                k = int(input("What would you like your value for the key to be: "))
+                new_dict[h]=k        
+    elif x == "no":
+        break
+print('This is your dictionary',new_dict)
