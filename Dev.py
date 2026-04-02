892e85e316d4588931176f56ff66ede339a9ed48
DevTamakuwala16 <devtamakuwala42@gmail.com>
2026-04-01 19:24:44 -0500

    Add files via upload

--- Dev.py
+++ Dev.py
@@ -1,37 +1,42 @@
-new_dict = {}
-while True:
-    print(new_dict)
-    x = input("Would you like to create a new key value pair: ").lower()
-    if x == 'yes':
-        d = input("Would you like your value to be a string or a number: ")
-        if d=='string':
-            y = input("Does this key have multiple values: ").lower()
-            if y == "yes":
-                z = input("What is the key: ").lower()
-                v = int(input("How many values would you like to add: "))
-                j = []
-                for i in range (v):
-                    b = input(f'{i+1} value: ')
-                    j.append(b)
-                new_dict[z]=j 
-            elif y =="no":
-                a = input("What would you like your key to be: ").lower()
-                c = input("What would you like your value for the key to be: ").lower()
-                new_dict[a]=c
-        if d == "number":
-            y = input("Does this key have multiple values: ").lower()
-            if y == "yes":
-                e = input("What is the key: ")
-                f = int(input("How many values would you like to add: "))
-                g = []
-                for i in range (f):
-                    b = input(f'{i+1} value: ')
-                    g.append(b)
-                new_dict[e]=g 
-            elif y =="no":
-                h = input("What would you like your key to be: ")
-                k = int(input("What would you like your value for the key to be: "))
-                new_dict[h]=k        
-    elif x == "no":
-        break
-print('This is your dictionary',new_dict)
+stack = [5, 4, 6, 7]
+def push(a):
+    stack.append(a)
+    
+push(8)
+print(stack)
+
+
+
+s = [5, 1, 3, 4, 7]
+def s_pop(s):
+    if len(s)==0:
+        return None
+    return s.pop()
+    
+for i in range(6):
+    s_pop()
+print(s)
+
+
+
+def empty(stack2):
+    return len(stack2)==0
+
+
+stack4 = []
+def push(b):
+    stack4.append(b)
+
+push(5)
+push(7)
+push(2)
+push(4)
+s_pop(stack4)
+push(8)
+push(1)
+push(1)
+push(3)    
+s_pop(stack4)                                                                                                     
+   
+
+    
