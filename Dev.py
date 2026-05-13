14a38bfe73945eb5f35c46c41df19738c6776973
DevTamakuwala16 <devtamakuwala42@gmail.com>
2026-05-12 19:00:54 -0500

    Add files via upload

--- Dev.py
+++ Dev.py
@@ -1,22 +1,66 @@
-import random
-fn = ["Cooper",
-      "Shaw",
-      "Porter",
-      "Mason"]
+player_name = input("Enter you hero's name: ")
+score = int(input("Enter your score: "))
 
-ln = ["Thomas",
-      "Siemon",
-      "Mays",
-      "Bradley"]
+with open ("Dev_score.txt", "w") as file
+    file.write(f"Player: {player_name}/n")
+    file.write(f"Score: {score}/n")
+    
+print("Your score has been saved")
 
-fi = open("Dev.txt", "w")
+try:
+    with open("Dev_score.txt", "r") as file
+    print("Memory Crystal Reveals: ")
+    print(file.read())
+except FileNotFoundError
+    print("No previous score found.")
 
-for i in range (6):
-    x = random.choice(fn)
-    y = random.choice(ln)
-    g_one=random.randint(50,100)
-    g_two=random.randint(60,100)
-    g_three=random.randint(70,100)
-    fi.write(f'{x} {y} {g_one} {g_two} {g_three}')
-    fi.close()
+try:
+    with open("Dev_score.txt", "r") as file
+    file.readline()
+    old_score = int(file.readline().split(":")[1])
+except FileNotFoundError:
+    old_score = 0
 
+earned = int(input("Points earned this round: "))
+new_score = old_score + earned
+
+with open("Dev_score.txt", "w") as file
+    file.write(f"Player: {player_name}/n")
+    file.write(f"Score: {new_score}/n")
+    
+print(f"Score updated to {new_score}")
+    
+current_score = int(input("Enter your score: "))
+
+try:
+    with open("Dev_score.txt", "r") as file
+        high_score = int(file.read())
+    except FileNotFoundError:
+        high_score = 0
+        
+if current_score > high_score:
+    with open("Dev_score.txt", "w") as file
+        file.write(str(current_score))
+    print("New High Score")
+else:
+    print(f"High Score to Beat: {high_score}")
+    
+player = input("Hero name: ")
+score = int(input("Score: "))
+
+scores = {}
+
+try:
+    with open("Dev_score.txt", "r") as file
+    for line in file:
+        name, pts = line.strip().split(":")
+        scores[name] = int(pts)
+    except FileNotFoundError:
+        pass
+    
+    scores[player] = score
+    
+    with open("Dev_score.txt", "w") as file
+    for name, pts in scores.items():
+
+      
